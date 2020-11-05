38296ab0ca2b4e737ba6051f3d568f471bf662ea
Ivan Diaz <ivanid22@gmail.com>
2020-11-05 14:11:12 -0300

    Add RecentWorkSectionItem component

--- src/components/RecentWorkSectionItem/RecentWorkSectionItem.js
+++ src/components/RecentWorkSectionItem/RecentWorkSectionItem.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from './RecentWorkSectionItem.module.scss';
 
-const RecentWorkSectionItem = ({ projectInfo, reversed }) => {
+const modalStyle = {
+  overlay: {
+    backgroundColor: 'inherit',
+  },
+  content: {
+    fontFamily: '\'Roboto\', sans-serif',
+    color: '#fff',
+    backgroundColor: 'rgba(255, 255, 255, 0.16)',
+    boxShadow: '0px 48px 48px rgba(37, 47, 137, 0.08)',
+    border: '1px solid #a2a3ae',
+    top: 20,
+    left: 20,
+    bottom: 20,
+    right: 20,
+  }
+}
 
+Modal.setAppElement('#root');
+
+const RecentWorkSectionItem = ({ projectInfo, reversed }) => {
+  const [modalOpen, setModalOpen] = useState(false);
   const { imageUrl, projectName, projectSummary } = projectInfo;
+  const appContainer = document.querySelector("#root"); // Get the app element from DOM to toggle a class. Awful hack, don't do it.
+
+  const showModal = () => {
+    appContainer.classList.add('blurred');
+    setModalOpen(true);
+  }
+  
+  const closeModal = () => {
+    appContainer.classList.remove('blurred');
+    setModalOpen(false);
+  }
+  
 
   return (
     <div className={style.itemContainer}>
@@ -13,7 +47,22 @@ const RecentWorkSectionItem = ({ projectInfo, reversed }) => {
       <div className={`col-12 col-md-4 ${reversed ? 'order-md-1' : undefined} ${style.infoContainer}`}>
         <h2 className={style.infoTitle}> {projectName} </h2>
         <p className={style.inforSummary}> {projectSummary} </p>
+        <button className={style.button} onClick={showModal}>View</button>
       </div>
+      <Modal style={modalStyle} isOpen={modalOpen} onRequestClose={closeModal}>
+        <div className={style.modalContentWrapper}>
+          <div className={style.modalRowOne}>
+            <h2> {projectName} </h2>
+            <button className={style.closeButton} onClick={closeModal}> 
+              <FontAwesomeIcon icon={faTimes} /> 
+            </button>
+          </div>
+          <img className={style.projectImg} src={imageUrl} alt="Project preview" />
+          <p className={style.modalDescriptionParagraph}>
+            {projectSummary}
+          </p>
+        </div>
+      </Modal>
     </div>
   )
 };
