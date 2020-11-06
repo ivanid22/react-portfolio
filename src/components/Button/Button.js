8e586014995fe5e5bb94a3e30499258ab6a1a429
Ivan Diaz <ivanid22@gmail.com>
2020-11-06 17:34:38 -0300

    Add contact form binding to API

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Button.module.scss';
 
 const Button = ({ enabled, onClick, children, width, type }) => {
-  console.log(styles);
   return(
     <button type={type ? type : 'button'} className={styles.button} disabled={!enabled} onClick={onClick} style={{width}}> 
       { children } 
--- src/components/ContactSectionForm/ContactSectionForm.js
+++ src/components/ContactSectionForm/ContactSectionForm.js
@@ -7,24 +7,87 @@ const ContactSectionForm = () => {
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [body, setBody] = useState('');
+  const [formState, setFormState] = useState('idle');
 
+  const resetFields = () => {
+    setEmail('');
+    setSubject('');
+    setBody('');
+  }
+  
+
+  const submitMessage = (event) => {
+    const {REACT_APP_BASE_MAIL_URL, REACT_APP_MAIL_API_DOMAIN, REACT_APP_MAIL_API_KEY, REACT_APP_MAIL_RECIPIENT } = process.env;
+    const requestUrl = `${REACT_APP_BASE_MAIL_URL}/${REACT_APP_MAIL_API_DOMAIN}/messages`;
+    event.preventDefault();
+    setFormState('sendingMessage');
+    /*axios({
+      url: requestUrl,
+      method: 'POST',
+      auth: {
+        username: 'api',
+        password: REACT_APP_MAIL_API_KEY,
+      },
+      params: {
+        from: email,
+        to: REACT_APP_MAIL_RECIPIENT,
+        subject: subject,
+        text: body,
+      },
+    }).then((response) => {
+      console.log(response)
+      setFormState('messageSent');
+    }).catch((error) => {
+      console.log(error)
+      setFormState('error');
+    });*/
+    setTimeout(() => {
+      resetFields();
+      setFormState('error');
+    }, 2000)
+  }
+
+  const renderConfirmation = () => (
+    <div className="col-12 d-flex justify-content-center">
+        <p className={styles.confirmationMessage}>Thank you for contacting me! I'll get back to you soon</p>
+    </div>
+  );
+
+  const appIdle = () => ((formState === 'idle') || (formState === 'messageSent') || (formState === 'error'))
+  
+  const renderError = () => (
+    <div className="col-12 d-flex justify-content-center">
+      <p className={styles.errorMessage}> There was an error sending the message, please try again later. </p>
+    </div>
+  )
+  
+  const renderForm = () => {
+    return (
+      <form className={styles.contactForm} onSubmit={submitMessage}>
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <input disabled={!appIdle()} className={`${styles.input} ${styles.emailInput}`} type="email" required placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="col-12 col-lg-6">
+            <input disabled={!appIdle()} className={`${styles.input} ${styles.subjectInput}`} type="text" required placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
+          </div>
+          <div className="col-12">
+            <textarea disabled={!appIdle()} className={`${styles.input} ${styles.bodyInput}`} required rows={10} placeholder="Message"  value={body} onChange={(e) => setBody(e.target.value)} />
+          </div>
+          { formState === 'messageSent' ? renderConfirmation() : undefined }
+          { formState === 'error' ? renderError() : undefined }
+          <div className="col-12 d-flex justify-content-center">
+            <Button enabled={appIdle()} type="submit">Submit message</Button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+  
   return (
-    <form className={styles.contactForm}>
-      <div className="row">
-      <div className="col-12 col-lg-6">
-        <input className={`${styles.input} ${styles.emailInput}`} type="email" required placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div className="col-12 col-lg-6">
-        <input className={`${styles.input} ${styles.subjectInput}`} type="text" required placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
-      </div>
-      <div className="col-12">
-        <textarea className={`${styles.input} ${styles.bodyInput}`} required rows={10} placeholder="Message"  value={body} onChange={(e) => setBody(e.target.value)} />
-      </div>
-      <div className="col-12">
-        <Button enabled type="submit">Submit</Button>
-      </div>
-      </div>
-    </form>
+    <div>
+      { renderForm() }
+    </div>
   )
 }
 
